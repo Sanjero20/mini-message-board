63ca98a51cfcbe7675f17bc1ec6d961a00ce6586
Sanjero20 <gelosantos72@gmail.com>
2023-09-13 23:48:35 +0800

    Refactor routes

--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -1,9 +1,27 @@
 import express, { Request, Response } from 'express';
-
-import newMessage from './new';
+import Message from '../models/message';
 
 const router = express.Router();
 
-router.use('/route', newMessage);
+router.get('/', async (req: Request, res: Response) => {
+  const response = await Message.find();
+  res.json(response);
+});
+
+router.post('/new', async (req: Request, res: Response) => {
+  const newMessage = new Message({
+    text: req.body.text,
+    user: req.body.user,
+    added: new Date().toISOString(),
+  });
+
+  try {
+    const response = await newMessage.save();
+    res.json(response);
+  } catch (error) {
+    console.log('Something went wrong', error);
+    res.status(500).json({ error });
+  }
+});
 
 export default router;
