92ce5e877b90ea70f0d562198f258905576d2344
Sanjero20 <gelosantos72@gmail.com>
2023-09-15 02:21:36 +0800

    Add response type

--- client/src/services/message.ts
+++ client/src/services/message.ts
@@ -1,15 +1,16 @@
-import { Message, NewMessage } from "@/types/messages";
+import { Message, NewMessage, UploadResponse } from "@/types/messages";
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || "";
 
-// Retrieve all messages
 export async function getAllMessages(): Promise<Message[]> {
   const response = await fetch(baseURL + "/");
   return await response.json();
 }
 
-export async function sendMessageToDB(data: NewMessage) {
-  const response = await fetch("http://localhost:5000/new", {
+export async function sendMessageToDB(
+  data: NewMessage,
+): Promise<UploadResponse> {
+  const response = await fetch(baseURL + "/new", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
--- client/src/types/messages.ts
+++ client/src/types/messages.ts
@@ -2,10 +2,15 @@ export type Message = {
   _id: string;
   user: string;
   text: string;
-  added: Date;
+  added: string;
 };
 
 export type NewMessage = {
   user: string;
   text: string;
+};
+
+export type UploadResponse = {
+  error: boolean;
+  data: Message | null;
 };
